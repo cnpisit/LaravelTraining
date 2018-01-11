c7f6f89acb06e5e9229c5abb8822135e79580c0f
cnpisit <chhornnypisit.itc@gmail.com>
2018-01-11 18:02:39 +0000

    [task] add more command

--- app/Http/Controllers/BookCustomController.php
+++ app/Http/Controllers/BookCustomController.php
@@ -12,6 +12,12 @@ class BookCustomController extends Controller
 		return view('book-custom.index')->with(['books'=>$books]);
 	}
 	
+	
+	/**
+	 * return book form
+	 * if don't have id will return blank form
+	 * if have id will return form with data
+	 * */
 	public function form($id=null){
 		if ($id!=null){
 			$book = Book::find($id);
@@ -20,6 +26,12 @@ class BookCustomController extends Controller
 		return view('book-custom.form');
 	}
 	
+	
+	/**
+	 * return book form
+	 * if don't have id mean create new
+	 * if have id mean update
+	 * */
 	public function save(Request $request){
 		$input = $request->input;
 		if ($request->has('id')){
@@ -32,6 +44,10 @@ class BookCustomController extends Controller
 		return redirect('/book-custom');
 	}
 	
+	
+	/**
+	 * delete book by id
+	 * */
 	public function delete($id){
 		Book::where('id',$id)->delete();
 		return redirect('/book-custom');
